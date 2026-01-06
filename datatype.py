ad986b8f90c0dde4fc1a2e15e11d70022c973c1f
drashtihadiyal-31 <drashtihadiyal295@gmail.com>
2026-01-06 08:13:41 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,24 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello")
+hello
+A=8
+print(A)
+8
+B=8.45
+print(B)
+8.45
+C=8j
+print(C)
+8j
+D=["apple","banana","mango"]
+print(D)
+['apple', 'banana', 'mango']
+E=("apple","banana","mango")
+print(E)
+('apple', 'banana', 'mango')
+F=True
+print(E)
+('apple', 'banana', 'mango')
+print(F)
+True
