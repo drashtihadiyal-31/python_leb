e75413c99e020dddcfecb1399b47df3e0449e556
drashtihadiyal-31 <drashtihadiyal295@gmail.com>
2026-01-31 08:26:48 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,67 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # arithmetic operator
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> # assignment operator
+>>> x=10
+>>> print(x)
+10
+>>> # unary minus
+>>> y=15
+>>> print(-y)
+-15
+>>> # relational operator
+>>> a=10
+>>> b=25
+>>> print(a<b)
+True
+>>> print(a==b)
+False
+>>> # logical operator
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> x=True
+>>> y=False
+>>> print(x or y)
+True
+>>> print(x and y)
+False
+>>> print(not x)
+False
+>>> # boolean operator
+>>> is-pass=True
+SyntaxError: invalid syntax
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> # bitwise operator
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a|b)
+7
+>>> # membership operator
+>>> numbers=[1,2,3,4]
+>>> print(3 in numbers)
+True
+>>> print(6 in numbers)
+False
+>>> #identity operator
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> 
