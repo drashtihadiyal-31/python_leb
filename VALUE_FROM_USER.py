15670950af7a3546ac31d4943172c4be1b32d22d
drashtihadiyal-31 <drashtihadiyal295@gmail.com>
2026-03-02 12:22:11 +0530

    Add files via upload

---
+++ VALUE_FROM_USER.py
@@ -0,0 +1,14 @@
+"""A=int(input("Enter value of A:"))
+B=int(input("Enter value of B:"))
+print("A+B=",A+B)
+print("A-B=",A-B)
+print("A*B=",A*B)
+print("A/B=",A/B)"""
+
+"""  SIMPAL INTEREST  """
+
+P=int(input("Enter principal:"))
+R=int(input("Enter Rate of interest:"))
+T=int(input("Enter Time:"))
+SI=P*R*T/100
+print("SI=P*R*T/100=",SI)
