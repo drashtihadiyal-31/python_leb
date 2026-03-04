77c435c98adff93808139801ef7fd1b98f780d9e
drashtihadiyal-31 <drashtihadiyal295@gmail.com>
2026-03-04 22:36:55 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,27 @@
+"""no=int(input("Enter no:"))
+if(no%2==0):
+    print("Even")
+else:
+    print("Odd") """
+    
+"""no=int(input("Enter no:"))
+if(no>0):
+    print("positive")
+elif(no<0):
+    print("Negative")   
+else:
+    print("Zero")"""
+    
+""" x=int(input("Enter the value of x:"))
+    y=int(input("Enter the value of y:"))
+if(x>y):
+    print("x is max")
+else:
+    print("y is max")  """   
+    
+"""  x=int(input("Enter the value of x:"))
+     y=int(input("Enter the value of y:"))
+if(x<y):
+    print("x is min")
+else:
+    print("y is min")      """    
